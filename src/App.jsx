0b4c529cada8088a8a7f869b98d74b4f35e219b6
Yess222 <jesus.zavaleta10@hotmail.com>
2024-02-25 23:12:18 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import AuthLayout from './layout/AuthLayout'
+import { RutaProtegia } from './layout/RutaProtegia'
+
+import { Login } from './paginas/Login'
+import { Registrar } from './paginas/Registrar'
+import { ConfirmarCuenta } from './paginas/ConfirmarCuenta'
+import { OlvidePassword } from './paginas/OlvidePassword'
+import { NuevoPassword } from './paginas/NuevoPassword'
+import { AdministrarPacientes } from './paginas/AdministrarPacientes'
+import EditarPerfil from './paginas/EditarPerfil'
+import CambiarPassword from './paginas/CambiarPassword'
+
+import { AuthProvider } from './context/AuthProvider'
+import { PacientesProvider } from './context/PacientesProvider'
+
+function App() {
+
+  return (
+    <BrowserRouter>
+        <AuthProvider>
+          <PacientesProvider>
+            <Routes>
+                {/* publica */}
+                <Route path='/' element={<AuthLayout/>}>
+                    <Route index element={<Login/>}/>
+                    <Route path='registrar' element={<Registrar/>}/>
+                    <Route path='olvide-password' element={<OlvidePassword/>}/>
+                    <Route path='olvide-password/:token' element={<NuevoPassword/>}/>
+                    <Route path='confirmar/:id' element={<ConfirmarCuenta/>}/>
+                </Route>
+                {/* Privado */}
+                <Route path='/admin' element={<RutaProtegia/>}>
+                    <Route index element={<AdministrarPacientes/>}/>
+                    <Route path='perfil' element={<EditarPerfil/>}/>
+                    <Route path='cambiar-password' element={<CambiarPassword/>}/>
+                </Route>
+            </Routes>
+          </PacientesProvider>
+        </AuthProvider>
+    </BrowserRouter>
+  )
+}
+
+export default App
